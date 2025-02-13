c394993299c240909f35bb5b390f4d02421231f2
Thomas Noel <tnoel@novadynamics.com>
2025-02-13 11:53:13 -0800

    Add extra visualization flag and benchmarking

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const StreamHistogram = require('./lib/StreamHistogram');
 !async function () {
     let args = process.argv.slice(2);
 
-    if (args.length > 1) {
+    if (args.length > 2) {
         throw new Error("Usage: node index.js <GRID-DIM>");
     }
 
     let grid_dim = args[0];
+    let visualize = !!args[1];
+
     const T = 2;
     const STEP_TIME = 0; // ms
 
@@ -18,19 +20,28 @@ const StreamHistogram = require('./lib/StreamHistogram');
 
     model.warmup();
 
-    let num_sweeps = 10000;
+    let num_sweeps = 1000;
     let data = [];
     
-    const runSweeps = async function* (ctx) {
-        for (let i = 0; i < num_sweeps; i++) {
-            await new Promise((res, rej) => setInterval(res, STEP_TIME));
-            yield {
-                context: ctx,
-                data: model.run_sweep()
-            };
+    if (visualize && grid_dim <= 3) {
+        const runSweeps = async function* (ctx) {
+            for (let i = 0; i < num_sweeps; i++) {
+                await new Promise((res, rej) => setInterval(res, STEP_TIME));
+                yield {
+                    context: ctx,
+                    data: model.run_sweep()
+                };
+            }
         }
-    }
 
-    const hist = new StreamHistogram(runSweeps);
-    hist.plot();
+        const hist = new StreamHistogram(runSweeps);
+        hist.plot();
+    } else {
+        const start = Date.now();
+        for (let i = 0; i < num_sweeps; i++) {
+            data.push(model.run_sweep());
+        }
+
+        console.log(`${num_sweeps} sweeps on a grid of size ${grid_dim}x${grid_dim} took ${(Date.now() - start) / 1000} seconds`);
+    }
 }()
