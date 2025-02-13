21c5213d4b2e9b900fec2467993b3b7e18a54631
Thomas Noel <thomas.c.noel@gmail.com>
2025-02-13 07:27:53 -0800

    Start dynamic plotting code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const process = require('process');
 const Ising = require('./lib/Ising');
+const Histogram = require('./lib/Histogram');
 
 !async function () {
     let args = process.argv.slice(2);
@@ -9,7 +10,7 @@ const Ising = require('./lib/Ising');
     }
 
     let grid_dim = args[0];
-    const T = 1;
+    const T = 2;
 
     let N = grid_dim ** 2;
     let model = new Ising({ grid_dim, T });
@@ -18,9 +19,11 @@ const Ising = require('./lib/Ising');
 
     let num_sweeps = 100;
     let data = [];
+
+    const hist = new Histogram(data);
+
     for (let i = 0; i < num_sweeps; i++) {
         data.push(model.run_sweep());
+        await new Promise((res, rej) => setInterval(res, 100));
     }
-
-    // console.log(data);
 }()
